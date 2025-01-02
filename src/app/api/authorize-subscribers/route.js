6666ec8a6d540ffcb208ce0fb80422f70e13eafb
softqwewasd <softqwewasd@protonmail.com>
2025-01-02 17:42:19 +0100

    Revert "Proof-of-concept IndexedDB + localstorage for caching events"
    This reverts commit 7cb09d303ceac7b5a6dd12d337af6d0646ae1416.

--- src/app/api/authorize-subscribers/route.js
+++ src/app/api/authorize-subscribers/route.js
@@ -1,14 +1,13 @@
 
 import * as exoquicAuth from "@exoquic/auth";
 import { NextResponse } from "next/server";
-import { v4 as uuidv4 } from "uuid";
+
 // Initialize the subscription authorizer with your API key
 exoquicAuth.initSubscriptionAuthorizer({ apiKey: process.env.EXOQUIC_API_KEY, serverUrl: `https://${process.env.EXOQUIC_ENV_CONTEXT}.exoquic.com` });
 
 // Since this is just a simple chat, we only have the topic "chat", so any call to
 // this endpoint will be authorized for the topic "chat".
 export async function GET(req) {
-	const subscriptionId = uuidv4();
 
 	// A subscriber that subscribes with this subscription token, will receive all the stored events, and any
 	// new events that are published to the topic "simple-chat".
